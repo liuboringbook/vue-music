5c4d6f608b892f44bc2bc8bca38d1936a16d4940
read a book <1287128847@qq.com>
2019-04-01 19:49:07 +0800

    singer-detail

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,13 +1,15 @@
 <template lang="html">
   <transition  name="slide">
-    <div class="singer-detail">
-    </div>
+    <music-list class="singer-detail" :songs="songs" :title="title" :bg-image="bgImage">
+    </music-list>
   </transition>
 </template>
 <script type="text/ecmascript-6">
    import {mapGetters} from 'vuex'
    import {getSingerDetail} from 'api/singer'
    import {ERR_OK} from 'api/config'
+   import {createSong} from 'common/js/song'
+   import  MusicList from 'components/music-list/music-list'
    export default {
      data(){
        return {
@@ -15,13 +17,18 @@
        }
      },
      computed: {
+       title(){
+         return this.singer.name
+       },
+       bgImage(){
+         return this.singer.avatar
+       },
        ...mapGetters([
          'singer'
        ])
      },
      created(){
        this._getDetail();
-       console.log(this.singer)
      },
      methods: {
        _getDetail(){
@@ -31,24 +38,29 @@
          }
          getSingerDetail(this.singer.id).then((res) =>{
             if(res.code === ERR_OK){
-              console.log(res.data)
+              this.songs =this._normalizeSongs(res.data.list)
             }
          })
+       },
+       _normalizeSongs(list){
+         let ret = []
+         list.forEach((item) =>{
+           let {musicData} = item
+           if(musicData.songid && musicData.songmid){
+             ret.push(createSong(musicData))
+           }
+         })
+         console.log(ret)
+         return ret
        }
+     },
+     components: {
+       MusicList
      }
    }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position:fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
   .slide-enter-active, .slider-leave-active
     transition: all 0.3s
   .slide-enter, .slide-leave-to
